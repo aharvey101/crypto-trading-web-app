ea78422dbc26cdb055ccf33f344ca3935184bfaf
Alexander <harvey.alex01@gmail.com>
2020-04-21 17:55:03 +1000

    added ccxt

--- routes/User.js
+++ routes/User.js
@@ -5,9 +5,26 @@ const middleware = require('../middleware/isLoggedIn.js')
 const moment = require('moment')
 const user = require('../models/user')
 const CCXT = require('ccxt')
+require('dotenv').config()
+
+const ftx = new CCXT.ftx({
+  apiKey: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
+
+const b = (async () => {
+  const balances = await ftx.fetchBalance()
+  let usdt = balances.total.USDT + balances.total.USD
+  return usdt
+})();
 
 router.get('/', middleware.isLoggedIn, function (req, res) {
-  res.render('user/profile')
+  async function awaitb(b) {
+    let ab = await b
+    console.log(ab)
+    res.render('user/profile', { balance: ab })
+  }
+  awaitb(b)
 })
 
 module.exports = router;
