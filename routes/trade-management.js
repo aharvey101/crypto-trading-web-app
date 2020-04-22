230c214bf030c13c1af55b9abd7fa3445a6f218b
Alexander <harvey.alex01@gmail.com>
2020-04-22 15:50:26 +1000

    added active trades

--- routes/trade-management.js
+++ routes/trade-management.js
@@ -4,13 +4,37 @@ const Trade = require('../models/trades');
 const middleware = require('../middleware/isLoggedIn.js')
 const moment = require('moment')
 const user = require('../models/user')
+const CCXT = require('ccxt')
+require('dotenv').config()
+
+//object for connecting
+const ftx = new CCXT.ftx({
+  apiKey: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
+
 
 router.get('/', middleware.isLoggedIn, (req, res) => {
-  Trade.find({ "author.id": req.user._id }, function (err, usersTrades) {
+  Trade.find({ "author.id": req.user._id }, function (err) {
+    const p = (async () => {
+      let pos = await ftx.private_get_positions()
+      return pos
+    })();
     if (err) {
       console.log(err)
     } else {
-      res.render('trade-management/index', { trade: usersTrades })
+      async function awaitp(p) {
+        let ap = await p
+        let newap = []
+        for (let i = 0; i < 3; i++) {
+          if (ap.result[i].size > 0) {
+            newap.push(ap.result[i])
+          }
+        }
+        console.log(newap)
+        res.render('trade-management/index', { pos: newap })
+      }
+      awaitp(p)
     }
   })
 })
